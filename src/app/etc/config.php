de00eee23a4764a3926372ffed92aefe47bcae83
Dung <tlbgtn@gmail.com>
2024-09-09 16:26:30 +0700

    update: enable Customer Products Module

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -373,6 +373,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'Ecentura_CustomerProducts' => 1,
         'Ecentura_FreeProduct' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeCustomerBalance' => 1,
